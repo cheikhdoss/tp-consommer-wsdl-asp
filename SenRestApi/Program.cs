cbba9c02d7b9a0dd9537f281cbbaccf0f7a1d352
cheikhdoss <cmbodj@groupeisi.com>
2026-08-05 01:20:55 +0000

    Add Swagger UI for API testing

--- SenRestApi/Program.cs
+++ SenRestApi/Program.cs
@@ -4,15 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-app.MapGet("/health", () => "OK");
 
 app.MapControllers();
 
